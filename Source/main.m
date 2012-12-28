a8d19a376abcbfd80bf923983e719ef098113599
Eric Broska <eric.broska@me.com>
2012-12-29 00:39:13 +0700

    Decrease code size; Skype.app now can be placed everywhere, not in /Applications only;

--- Source/main.m
+++ Source/main.m
@@ -5,129 +5,26 @@
 //  Created by Alexander Holodny on 11/27/12.
 //  Copyright (c) 2012 Alexander Holodny. All rights reserved.
 //
+//
 
-AuthorizationRef _authorizationRef;
 
-BOOL acquirePrivileges();
-void releasePrivileges();
+int main(int argc, const char *argv[]) {
+    @autoreleasepool {
+        /*
+        * Skype reads Skype.pid file for a pid of an already launched copy on startup,
+        * so if we remove this file, Skype won't be panic with the "You have another copy of Skype running"
+        * message.
+        */
+        [[NSFileManager defaultManager] removeItemAtPath:[@"~/Library/Application Support/Skype/Skype.pid" stringByExpandingTildeInPath]
+                                                   error:NULL];
 
-int main(int argc, const char * argv[])
-{
-	@autoreleasepool
-	{
-		NSArray *apps = [[NSWorkspace sharedWorkspace] runningApplications];
-		NSString *skype = @"/Applications/Skype.app/Contents/MacOS/Skype";
-		pid_t loginWindowPid = 0, skypePid = 0;
-		
-		if ([[NSFileManager defaultManager] fileExistsAtPath:skype] == NO)
-		{
-			[[NSAlert alertWithMessageText:@"Error"
-							 defaultButton:@"Quit"
-						   alternateButton:nil
-							   otherButton:nil
-				 informativeTextWithFormat:@"Skype app not found"]
-			 runModal];
-			
-			return -1;
-		}
-		
-		for (NSRunningApplication *app in apps)
-		{
-			if ([app.bundleIdentifier isEqualToString:@"com.apple.loginwindow"])
-			{
-				loginWindowPid = app.processIdentifier;
-			}
-			
-			if ([app.bundleIdentifier isEqualToString:@"com.skype.skype"])
-			{
-				skypePid = app.processIdentifier;
-			}
-		}
-		
-		if ( 0 == skypePid )
-		{
-			[[NSWorkspace sharedWorkspace] launchApplicationAtURL:[NSURL fileURLWithPath:skype]
-														  options:0
-													configuration:NULL
-															error:NULL];
-		}
-		else if ( 0 != loginWindowPid)
-		{
-			char pid[16] = {0};
-			
-			snprintf(pid, sizeof(pid), "%d", loginWindowPid);
-			
-			if (acquirePrivileges())
-			{
-				FILE *pipe = NULL;
-				char * args[] = {
-					"-u",
-					"root",
-					"/",
-					"launchctl",
-					"bsexec",
-					NULL,
-					(char*)skype.fileSystemRepresentation, NULL};
-				
-				args[5] = (char*)&pid;
-				
-				AuthorizationExecuteWithPrivileges(_authorizationRef, "/usr/sbin/chroot", kAuthorizationFlagDefaults, args, &pipe);
-				
-				releasePrivileges();
-				return 0;
-			}
-		}
-		else
-		{
-			[[NSAlert alertWithMessageText:@"Error"
-							 defaultButton:@"Quit"
-						   alternateButton:nil
-							   otherButton:nil
-				 informativeTextWithFormat:@"No loginwindow found"]
-			 runModal];
-		}
-	}
-	
-    return -1;
-}
+        NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+        NSURL *skype_url = [NSURL fileURLWithPath:
+                [workspace absolutePathForAppBundleWithIdentifier:@"com.skype.skype"]];
 
-BOOL acquirePrivileges()
-{
-	AuthorizationItem authItems = {kAuthorizationRightExecute, 0, NULL, 0};
-	AuthorizationRights authRights = {1, &authItems};
-	OSStatus status = 0;
-	
-	status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &_authorizationRef);
-	
-	if ( errAuthorizationSuccess != status )
-	{
-		NSLog(@"AuthorizationCreate failed with %d", (int)status);
-		return NO;
-	}
-	
-	status = AuthorizationCopyRights(_authorizationRef, &authRights, NULL,
-									 kAuthorizationFlagDefaults | kAuthorizationFlagInteractionAllowed |
-									 kAuthorizationFlagPreAuthorize | kAuthorizationFlagExtendRights, NULL);
-	
-	if ( errAuthorizationSuccess != status )
-	{
-		NSLog(@"AuthorizationCopyRights failed with %d", (int)status);
-		releasePrivileges();
-		return NO;
-	}
-	
-	return YES;
-}
-
-void releasePrivileges()
-{
-	if (_authorizationRef)
-	{
-		OSStatus status = AuthorizationFree(_authorizationRef, kAuthorizationFlagDestroyRights);
-		
-		if ( errAuthorizationSuccess != status)
-			NSLog(@"AuthorizationFree failed with %d", (int)status);
-		
-		_authorizationRef = NULL;
-	}
+        [workspace launchApplicationAtURL:skype_url
+                                  options:NSWorkspaceLaunchNewInstance
+                            configuration:nil error:NULL];
+    }
+    return 0;
 }
